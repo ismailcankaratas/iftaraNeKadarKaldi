0793b0c66432f0c538e40f79a422930ea0bf4a34
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2022-04-08 16:01:11 +0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        pageTitle: "url('https://iftaranekadarkaldi.com/pattern/iftar_light.svg')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
